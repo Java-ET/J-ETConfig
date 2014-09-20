130014c26123f52739f56e36b0cb1f4dad44ca64
Ziggeh <ziggy@projectswg.com>
2014-09-20 06:23:12 +0200

    Added setEntry() method to Config class
    This will allow the modification of keys and values.

--- src/utils/Config.java
+++ src/utils/Config.java
@@ -7,12 +7,12 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Config {
-	private Map<String, Object> entries;
-	
-	public Config(String configPath) {	
-		entries = new HashMap<String, Object>();
-		File cfgFile = new File(configPath);
-		
+	private Map<String, Object> entries = new HashMap<String, Object>();
+	private File cfgFile;
+
+	public Config(String configPath) {
+		cfgFile = new File(configPath);
+
 		Scanner fileReader = null;
 		try {
 			fileReader = new Scanner(cfgFile);
@@ -20,28 +20,34 @@ public class Config {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		while(fileReader.hasNextLine()) {
-			String entry  = fileReader.nextLine();
 
-			entries.put(entry.split("=")[0], entry.split("=")[1]);	// Cache the config. More efficient than reading it over and over.
+		// Cache the config. More efficient than reading it over and over.
+		while (fileReader.hasNextLine()) {
+			String entry = fileReader.nextLine();
+			entries.put(entry.split("=")[0], entry.split("=")[1]);
 		}
+
 		fileReader.close();
 	}
-	
+
 	public int getInt(String key) {
 		return Integer.valueOf((String) entries.get(key));
 	}
-	
+
 	public boolean getBoolean(String key) {
 		return Boolean.getBoolean((String) entries.get(key));
 	}
-	
+
 	public String getString(String key) {
 		return (String) entries.get(key);
 	}
-	
+
 	public byte getByte(String key) {
 		return Byte.valueOf((String) entries.get(key));
 	}
+
+	public void setEntry(String key, Object value) {
+		entries.put(key, value);
+	}
+
 }
